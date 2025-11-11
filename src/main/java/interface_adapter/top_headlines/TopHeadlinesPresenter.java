8adae2bcf268975102356d87c7932deb32f31b0b
berkebasak <berke.basak@mail.utoronto.ca>
2025-11-10 23:40:42 -0500

    Implemented the Topheadlines Presenter to format the data to the user interface

--- src/main/java/interface_adapter/top_headlines/TopHeadlinesPresenter.java
+++ src/main/java/interface_adapter/top_headlines/TopHeadlinesPresenter.java
@@ -1,4 +1,17 @@
-package interface_adapter;
+package interface_adapter.top_headlines;
 
-public class TopHeadlinesPresenter {
+import use_case.top_headlines.TopHeadlinesOutputBoundary;
+import use_case.top_headlines.TopHeadlinesOutputData;
+
+public class TopHeadlinesPresenter implements TopHeadlinesOutputBoundary {
+    private final TopHeadlinesViewModel viewModel;
+
+    public TopHeadlinesPresenter(TopHeadlinesViewModel viewModel) {
+        this.viewModel = viewModel;
+    }
+
+    @Override
+    public void present(TopHeadlinesOutputData outputData) {
+        viewModel.getState().setArticles(outputData.getArticles());
+    }
 }
