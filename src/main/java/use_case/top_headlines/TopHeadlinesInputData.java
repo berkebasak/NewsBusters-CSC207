1b9d0bcbb61cd5dcb9249c41b0638ca25793753d
berkebasak <berke.basak@mail.utoronto.ca>
2025-11-10 23:36:51 -0500

    Implemented the input data class for TopHeadlines interactor

--- src/main/java/use_case/top_headlines/TopHeadlinesInputData.java
+++ src/main/java/use_case/top_headlines/TopHeadlinesInputData.java
@@ -1,4 +1,14 @@
-package use_case;
+package use_case.top_headlines;
 
 public class TopHeadlinesInputData {
+    // This can later include parameters like category or country
+    private final String requestType;
+
+    public TopHeadlinesInputData(String requestType) {
+        this.requestType = requestType;
+    }
+
+    public String getRequestType() {
+        return requestType;
+    }
 }
