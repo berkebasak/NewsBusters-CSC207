e8e17751ba71aa483223d4c9f14b204a2167334d
berkebasak <berke.basak@mail.utoronto.ca>
2025-11-10 23:38:06 -0500

    Implemented the data access interface for TopHeadlines use case

--- src/main/java/use_case/top_headlines/TopHeadlinesUserDataAccessInterface.java
+++ src/main/java/use_case/top_headlines/TopHeadlinesUserDataAccessInterface.java
@@ -1,4 +1,8 @@
-package use_case;
+package use_case.top_headlines;
 
-public class TopHeadlinesUserDataAccessInterface {
+import entity.Article;
+import java.util.List;
+
+public interface TopHeadlinesUserDataAccessInterface {
+    List<Article> fetchTopHeadlines();
 }
