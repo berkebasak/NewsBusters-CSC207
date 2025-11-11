09d4cf68bac369cdffc9341f798dc0d6277232ff
berkebasak <berke.basak@mail.utoronto.ca>
2025-11-10 23:39:53 -0500

    Implemented the TopHeadlines Controller to trigger the use case from the view

--- src/main/java/interface_adapter/top_headlines/TopHeadlinesController.java
+++ src/main/java/interface_adapter/top_headlines/TopHeadlinesController.java
@@ -1,4 +1,17 @@
-package interface_adapter;
+package interface_adapter.top_headlines;
+
+import use_case.top_headlines.TopHeadlinesInputBoundary;
+import use_case.top_headlines.TopHeadlinesInputData;
 
 public class TopHeadlinesController {
+    private final TopHeadlinesInputBoundary interactor;
+
+    public TopHeadlinesController(TopHeadlinesInputBoundary interactor) {
+        this.interactor = interactor;
+    }
+
+    public void fetchHeadlines() {
+        TopHeadlinesInputData inputData = new TopHeadlinesInputData("top");
+        interactor.execute(inputData);
+    }
 }
