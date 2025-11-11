c525c0659e7fc82608ab4345ed702cd0936d1fce
berkebasak <berke.basak@mail.utoronto.ca>
2025-11-10 23:41:24 -0500

    Implemented the topheadlines state

--- src/main/java/interface_adapter/top_headlines/TopHeadlinesState.java
+++ src/main/java/interface_adapter/top_headlines/TopHeadlinesState.java
@@ -1,4 +1,17 @@
-package interface_adapter;
+package interface_adapter.top_headlines;
+
+import entity.Article;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TopHeadlinesState {
+    private List<Article> articles = new ArrayList<>();
+
+    public List<Article> getArticles() {
+        return articles;
+    }
+
+    public void setArticles(List<Article> articles) {
+        this.articles = articles;
+    }
 }
