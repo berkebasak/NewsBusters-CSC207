044de712b77613d038e5ecfcb93d74f5daa2bebf
Lana Nguyen <nnlanann@gmail.com>
2025-11-11 19:34:20 -0500

    Implemented input/output data classes - move data between ui/use case layers

--- src/main/java/use_case/search_news/SearchNewsInputData.java
+++ src/main/java/use_case/search_news/SearchNewsInputData.java
@@ -1,4 +1,26 @@
 package use_case.search_news;
 
+/**
+ * The Input Data for the Search News by Keyword Use Case.
+ * Holds the keyword entered by the user for searching articles.
+ */
 public class SearchNewsInputData {
+
+    private final String keyword;
+
+    /**
+     * Creates a new SearchNewsInputData object, input data for the search.
+     * @param keyword the keyword entered by the user
+     */
+    public SearchNewsInputData(String keyword) {
+        this.keyword = keyword;
+    }
+
+    /**
+     * Gets the keyword entered by the user.
+     * @return the keyword as a String
+     */
+    public String getKeyword() {
+        return keyword;
+    }
 }
--- src/main/java/use_case/search_news/SearchNewsOutputData.java
+++ src/main/java/use_case/search_news/SearchNewsOutputData.java
@@ -1,4 +1,39 @@
 package use_case.search_news;
 
+import java.util.List;
+import entity.Article;
+
+/**
+ * Output Data for the Login Use Case.
+ * Holds the search results and keyword.
+ */
 public class SearchNewsOutputData {
+    private final String keyword;
+    private final List<Article> articles;
+
+    /**
+     * Creates a new SearchNewsOutputData object, output data with search results.
+     * @param keyword  the search keyword used
+     * @param articles the list of articles matching the keyword
+     */
+    public SearchNewsOutputData(String keyword, List<Article> articles) {
+        this.keyword = keyword;
+        this.articles = articles;
+    }
+
+    /**
+     * Gets the keyword that was searched.
+     * @return the search keyword
+     */
+    public String getKeyword() {
+        return keyword;
+    }
+
+    /**
+     * Gets the list of articles that matched the keyword.
+     * @return a list of Article entities
+     */
+    public List<Article> getArticles() {
+        return articles;
+    }
 }
