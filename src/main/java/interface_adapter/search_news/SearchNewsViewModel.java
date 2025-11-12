051b382e2d6aa0fc29f253695ef33b8780baff78
Lana Nguyen <nnlanann@gmail.com>
2025-11-11 21:07:51 -0500

    Implemented SearchNewsViewModel.java to manage states - Manages UI state and notifies view when state changes

--- src/main/java/interface_adapter/search_news/SearchNewsViewModel.java
+++ src/main/java/interface_adapter/search_news/SearchNewsViewModel.java
@@ -1,4 +1,45 @@
 package interface_adapter.search_news;
 
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+/**
+ * The ViewModel for the Search News feature.
+ */
+
 public class SearchNewsViewModel {
+
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+    private SearchNewsState state = new SearchNewsState();
+
+    /**
+     * @return the current state object
+     */
+    public SearchNewsState getState() {
+        return state;
+    }
+
+    /**
+     * Replace the current state with a new one.
+     * @param newState new state from the Presenter
+     */
+    public void setState(SearchNewsState newState) {
+        this.state = newState;
+    }
+
+    /**
+     * Tells listeners that the state changed.
+     * @param propertyName the label for the property that was changed
+     */
+    public void firePropertyChanged(String propertyName) {
+        this.support.firePropertyChange(propertyName, null, this.state);
+    }
+
+    /**
+     * Add a UI listener to receive updates.
+     * @param listener a PropertyChangeListener
+     */
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        this.support.addPropertyChangeListener(listener);
+    }
 }
