36d0ffb1a3ca134bb857c61203a9d17134ade39e
Lana Nguyen <nnlanann@gmail.com>
2025-11-11 19:22:58 -0500

    Add input/out boundaries for search use case - defined to abstract interactor communication

--- src/main/java/use_case/search_news/SearchNewsInputBoundary.java
+++ src/main/java/use_case/search_news/SearchNewsInputBoundary.java
@@ -1,4 +1,14 @@
 package use_case.search_news;
 
-public class SearchNewsInputBoundary {
+/**
+ * The Input Boundary for the Search News use case.
+ * Called by the Controller to start the search.
+ */
+public interface SearchNewsInputBoundary {
+
+    /**
+     * Starts the search with user input.
+     * @param inputData keyword entered by the user
+     */
+    void execute(SearchNewsInputData inputData);
 }
--- src/main/java/use_case/search_news/SearchNewsOutputBoundary.java
+++ src/main/java/use_case/search_news/SearchNewsOutputBoundary.java
@@ -1,4 +1,21 @@
 package use_case.search_news;
 
-public class SearchNewsOutputBoundary {
+/**
+ * Output Boundary interface for the Search News use case.
+ * Used by the Interactor to send results to the Presenter.
+ */
+public interface SearchNewsOutputBoundary {
+
+    /**
+     * Shows the successful search results.
+     * @param outputData the data containing the search results
+     */
+    void prepareSuccessView(SearchNewsOutputData outputData);
+
+    /**
+     * Shows an error message if the search fails.
+     * @param message the error message to display
+     */
+    void prepareFailView(String message);
+
 }
