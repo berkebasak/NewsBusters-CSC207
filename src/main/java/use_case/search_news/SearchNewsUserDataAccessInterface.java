af57c70d4d043ab8a00fde7e3504b6d39c0fae3b
Lana Nguyen <nnlanann@gmail.com>
2025-11-11 19:52:15 -0500

    Add SearchNewsDataAccessInterface - interface that specifys how the interactor retrieves articles by keyword

--- src/main/java/use_case/search_news/SearchNewsUserDataAccessInterface.java
+++ src/main/java/use_case/search_news/SearchNewsUserDataAccessInterface.java
@@ -5,7 +5,15 @@ import entity.Article;
 
 /**
  * DAO interface for Searching News by Keyword Use Case.
+ * Gets articles from a data source
  */
 public interface SearchNewsUserDataAccessInterface {
+
+    /**
+     * Searches for articles by keyword.
+     * @param keyword keyword to search for
+     * @return list of matching articles
+     * @throws Exception if data retrieval fails
+     */
     List<Article> searchByKeyword(String keyword) throws Exception;
 }
