6cb0d37d7c4e638ca46d9e2a1735308c5d38441e
Avantika <avantikachat@gmail.com>
2025-11-15 16:56:08 -0500

    implemented input/output boundary classes

--- src/main/java/use_case/discover_page/DiscoverPageInputBoundary.java
+++ src/main/java/use_case/discover_page/DiscoverPageInputBoundary.java
@@ -1,4 +1,5 @@
 package use_case.discover_page;
 
 public interface DiscoverPageInputBoundary {
+    void execute(DiscoverPageInputData inputData);
 }
--- src/main/java/use_case/discover_page/DiscoverPageOutputBoundary.java
+++ src/main/java/use_case/discover_page/DiscoverPageOutputBoundary.java
@@ -1,4 +1,9 @@
 package use_case.discover_page;
 
 public interface DiscoverPageOutputBoundary {
+    void prepareSuccessView(DiscoverPageOutputData response);
+
+    void prepareNoHistoryView(String message);
+
+    void prepareNoArticlesView(String message);
 }
