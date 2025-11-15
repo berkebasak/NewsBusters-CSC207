1fa513cd66723eaad00f9a578d046f047ed960ff
Avantika <avantikachat@gmail.com>
2025-11-15 18:01:00 -0500

    implemented DataAccessInterface class

--- src/main/java/use_case/discover_page/DiscoverPageDataAccessInterface.java
+++ src/main/java/use_case/discover_page/DiscoverPageDataAccessInterface.java
@@ -1,4 +1,33 @@
 package use_case.discover_page;
 
+import entity.Article;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Data Access Interface for the Discover Page use case.
+ * Provides methods to access reading history and search for articles by topics.
+ */
 public interface DiscoverPageDataAccessInterface {
+
+    /**
+     * Gets all saved articles (reading history) from storage.
+     * @return list of saved articles, or empty list if none exist
+     */
+    List<Article> getReadingHistory();
+
+    /**
+     * Extracts top topics from a list of articles based on word frequency in titles.
+     * @param articles the articles to analyze
+     * @param topN the number of top topics to return
+     * @return set of top topic keywords
+     */
+    Set<String> extractTopTopics(List<Article> articles, int topN);
+
+    /**
+     * Searches for new articles related to the given topics.
+     * @param topics the topics to search for
+     * @return list of articles related to the topics
+     */
+    List<Article> searchByTopics(Set<String> topics);
 }
