e88725faeb204fa77284b6729ac0196f2be83769
Avantika <avantikachat@gmail.com>
2025-11-15 21:44:29 -0500

    implemented DiscoverPageOutputData

--- src/main/java/use_case/discover_page/DiscoverPageOutputData.java
+++ src/main/java/use_case/discover_page/DiscoverPageOutputData.java
@@ -1,4 +1,24 @@
 package use_case.discover_page;
 
+import java.util.List;
+import java.util.Set;
+
+import entity.Article;
+
 public class DiscoverPageOutputData {
+    private final List<Article> articles;
+    private final Set<String> topics;
+
+    public DiscoverPageOutputData(List<Article> articles, Set<String> topics) {
+        this.articles = articles;
+        this.topics = topics;
+    }
+
+    public List<Article> getArticles() {
+        return articles;
+    }
+
+    public Set<String> getTopics() {
+        return topics;
+    }
 }
