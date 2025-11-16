8b8398f02de853e18014fdc469f054f2dd246fc8
Avantika <avantikachat@gmail.com>
2025-11-15 22:53:14 -0500

    deletion of fields in DiscoverPageViewModel

--- src/main/java/interface_adapter/discover_page/DiscoverPageViewModel.java
+++ src/main/java/interface_adapter/discover_page/DiscoverPageViewModel.java
@@ -4,8 +4,6 @@ import interface_adapter.ViewModel;
 
 public class DiscoverPageViewModel extends ViewModel<DiscoverPageState> {
 
-    public static final String VIEW_NAME = "discover_page_view";
-
     public DiscoverPageViewModel() {
         super("discover page");
         setState(new DiscoverPageState());
