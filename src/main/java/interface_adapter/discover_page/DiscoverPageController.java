fda6f730d3daaa56ee33621ce24f08e5d11e07c2
Avantika <avantikachat@gmail.com>
2025-11-15 23:12:04 -0500

    implemented DiscoverPageController

--- src/main/java/interface_adapter/discover_page/DiscoverPageController.java
+++ src/main/java/interface_adapter/discover_page/DiscoverPageController.java
@@ -1,4 +1,18 @@
 package interface_adapter.discover_page;
 
+import use_case.discover_page.DiscoverPageInputBoundary;
+import use_case.discover_page.DiscoverPageInputData;
+
 public class DiscoverPageController {
+    private final DiscoverPageInputBoundary discoverPageUseCaseInteractor;
+
+    public DiscoverPageController(DiscoverPageInputBoundary discoverPageUseCaseInteractor) {
+        this.discoverPageUseCaseInteractor = discoverPageUseCaseInteractor;
+    }
+
+    public void execute() {
+        DiscoverPageInputData inputData = new DiscoverPageInputData();
+        discoverPageUseCaseInteractor.execute(inputData);
+    }
 }
+
