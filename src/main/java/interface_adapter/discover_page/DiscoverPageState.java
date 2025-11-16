a07691247c1c6a637e55c04d6dcaaa5379662787
Avantika <avantikachat@gmail.com>
2025-11-15 23:04:27 -0500

    implemented DiscoverPageState

--- src/main/java/interface_adapter/discover_page/DiscoverPageState.java
+++ src/main/java/interface_adapter/discover_page/DiscoverPageState.java
@@ -1,4 +1,45 @@
 package interface_adapter.discover_page;
 
+import entity.Article;
+import java.util.ArrayList;
+import java.util.List;
+
 public class DiscoverPageState {
+    private List<Article> articles = new ArrayList<>();
+    private String message;
+    private boolean hasNoHistory;
+    private boolean hasNoArticles;
+
+    public List<Article> getArticles() {
+        return articles;
+    }
+
+    public void setArticles(List<Article> articles) {
+        this.articles = articles;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public boolean getHasNoHistory() {
+        return hasNoHistory;
+    }
+
+    public void setHasNoHistory(boolean hasNoHistory) {
+        this.hasNoHistory = hasNoHistory;
+    }
+
+    public boolean getHasNoArticles() {
+        return hasNoArticles;
+    }
+
+    public void setHasNoArticles(boolean hasNoArticles) {
+        this.hasNoArticles = hasNoArticles;
+    }
 }
+
