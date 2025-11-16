463e2d32ccee9a0c907bda41f4c63a2ee5498bf3
Avantika <avantikachat@gmail.com>
2025-11-15 23:26:10 -0500

    implemented DiscoverPagePresenter

--- src/main/java/interface_adapter/discover_page/DiscoverPagePresenter.java
+++ src/main/java/interface_adapter/discover_page/DiscoverPagePresenter.java
@@ -1,4 +1,46 @@
 package interface_adapter.discover_page;
 
-public class DiscoverPagePresenter {
+import use_case.discover_page.DiscoverPageOutputBoundary;
+import use_case.discover_page.DiscoverPageOutputData;
+
+import java.util.Collections;
+
+public class DiscoverPagePresenter implements DiscoverPageOutputBoundary {
+
+    private final DiscoverPageViewModel discoverPageViewModel;
+
+    public DiscoverPagePresenter(DiscoverPageViewModel discoverPageViewModel) {
+        this.discoverPageViewModel = discoverPageViewModel;
+    }
+
+    @Override
+    public void prepareSuccessView(DiscoverPageOutputData outputData) {
+        DiscoverPageState state = discoverPageViewModel.getState();
+        state.setArticles(outputData.getArticles());
+        state.setMessage(null);
+        state.setHasNoHistory(false);
+        state.setHasNoArticles(false);
+        discoverPageViewModel.firePropertyChange();
+    }
+
+    @Override
+    public void prepareNoHistoryView(String message) {
+        DiscoverPageState state = discoverPageViewModel.getState();
+        state.setMessage(message);
+        state.setHasNoHistory(true);
+        state.setHasNoArticles(false);
+        state.setArticles(Collections.emptyList());
+        discoverPageViewModel.firePropertyChange();
+    }
+
+    @Override
+    public void prepareNoArticlesView(String message) {
+        DiscoverPageState state = discoverPageViewModel.getState();
+        state.setMessage(message);
+        state.setHasNoArticles(true);
+        state.setHasNoHistory(false);
+        state.setArticles(Collections.emptyList());
+        discoverPageViewModel.firePropertyChange();
+    }
 }
+
