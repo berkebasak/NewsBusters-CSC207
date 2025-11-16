f7d46d841171264807d5e75c737b7d3561769e3a
Avantika <avantikachat@gmail.com>
2025-11-15 22:41:22 -0500

    implemented DiscoverPageViewModel

--- src/main/java/interface_adapter/discover_page/DiscoverPageViewModel.java
+++ src/main/java/interface_adapter/discover_page/DiscoverPageViewModel.java
@@ -1,4 +1,14 @@
 package interface_adapter.discover_page;
 
-public class DiscoverPageViewModel {
+import interface_adapter.ViewModel;
+
+public class DiscoverPageViewModel extends ViewModel<DiscoverPageState> {
+
+    public static final String VIEW_NAME = "discover_page_view";
+
+    public DiscoverPageViewModel() {
+        super("discover page");
+        setState(new DiscoverPageState());
+    }
 }
+
