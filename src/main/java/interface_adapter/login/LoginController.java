cd73a7cfc2aa90bdf5d343601a4ec3ada6bd2bca
Halomaster0 <Halomaster2006lol@gmail.com>
2025-11-17 05:12:04 -0500

    Implemented LoginController.java (Interface)

---
+++ src/main/java/interface_adapter/login/LoginController.java
@@ -0,0 +1,17 @@
+package interface_adapter.login;
+
+import use_case.login.LoginInputBoundary;
+import use_case.login.LoginInputData;
+
+public class LoginController {
+    private final LoginInputBoundary interactor;
+
+    public LoginController(LoginInputBoundary interactor) {
+        this.interactor = interactor;
+    }
+
+    public void login(String username, String password) {
+        LoginInputData inputData = new LoginInputData(username, password);
+        interactor.execute(inputData);
+    }
+}
