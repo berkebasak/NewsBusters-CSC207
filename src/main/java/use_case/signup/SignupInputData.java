b602fa32ac0de11a09495c0e38dac363316a8e6e
Halomaster0 <Halomaster2006lol@gmail.com>
2025-11-17 06:46:54 -0500

    Implemented SignupInputData.java for Signup functionality

---
+++ src/main/java/use_case/signup/SignupInputData.java
@@ -0,0 +1,25 @@
+package use_case.signup;
+
+public class SignupInputData {
+    private final String username;
+    private final String password;
+    private final String confirmPassword;
+
+    public SignupInputData(String username, String password, String confirmPassword) {
+        this.username = username;
+        this.password = password;
+        this.confirmPassword = confirmPassword;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+}
