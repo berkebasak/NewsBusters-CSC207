77787a5900640c0e0642b94ebf155fb1684000fa
Halomaster0 <Halomaster2006lol@gmail.com>
2025-11-17 07:03:40 -0500

    Implemented Login/SignUp Screen size difference in ViewManager.java

--- src/main/java/view/ViewManager.java
+++ src/main/java/view/ViewManager.java
@@ -8,9 +8,13 @@ import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
 public class ViewManager implements PropertyChangeListener {
+    private static final Dimension LOGIN_SIZE = new Dimension(720, 520);
+    private static final Dimension DEFAULT_APP_SIZE = new Dimension(1100, 720);
+
     private final CardLayout cardLayout;
     private final JPanel views;
     private final ViewManagerModel viewManagerModel;
+    private JFrame hostFrame;
 
     public ViewManager(JPanel views, CardLayout cardLayout, ViewManagerModel viewManagerModel) {
         this.views = views;
@@ -19,11 +23,31 @@ public class ViewManager implements PropertyChangeListener {
         this.viewManagerModel.addPropertyChangeListener(this);
     }
 
+    public void setHostFrame(JFrame hostFrame) {
+        this.hostFrame = hostFrame;
+        this.hostFrame.setSize(LOGIN_SIZE);
+    }
+
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
-        if (evt.getPropertyName().equals("state")) {
+        if ("state".equals(evt.getPropertyName())) {
             String viewName = (String) evt.getNewValue();
             cardLayout.show(views, viewName);
+
+            if (hostFrame != null) {
+                SwingUtilities.invokeLater(() -> {
+                    Dimension targetSize;
+                    if (LoginView.VIEW_NAME.equals(viewName) || SignupView.VIEW_NAME.equals(viewName)) {
+                        targetSize = LOGIN_SIZE;
+                    } else {
+                        targetSize = DEFAULT_APP_SIZE;
+                    }
+                    if (!hostFrame.getSize().equals(targetSize)) {
+                        hostFrame.setSize(targetSize);
+                        hostFrame.setLocationRelativeTo(null);
+                    }
+                });
+            }
         }
     }
 }
