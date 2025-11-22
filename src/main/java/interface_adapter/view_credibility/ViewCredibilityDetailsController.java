35aaadb2650a04e72977a924eb22f25f06a93e86
berkebasak <berke.basak@mail.utoronto.ca>
2025-11-22 14:30:55 -0500

    Implemented the InterfaceAdapter layer for the View Credibility Use Case.

--- src/main/java/interface_adapter/view_credibility/ViewCredibilityDetailsController.java
+++ src/main/java/interface_adapter/view_credibility/ViewCredibilityDetailsController.java
@@ -1,4 +1,20 @@
 package interface_adapter.view_credibility;
 
+import entity.Article;
+import use_case.view_credibility.ViewCredibilityDetailsInputBoundary;
+import use_case.view_credibility.ViewCredibilityDetailsInputData;
+
 public class ViewCredibilityDetailsController {
+
+    private final ViewCredibilityDetailsInputBoundary interactor;
+
+    public ViewCredibilityDetailsController(ViewCredibilityDetailsInputBoundary interactor) {
+        this.interactor = interactor;
+    }
+
+    public void showDetails(Article article) {
+        ViewCredibilityDetailsInputData inputData =
+                new ViewCredibilityDetailsInputData(article);
+        interactor.execute(inputData);
+    }
 }
--- src/main/java/interface_adapter/view_credibility/ViewCredibilityDetailsPresenter.java
+++ src/main/java/interface_adapter/view_credibility/ViewCredibilityDetailsPresenter.java
@@ -1,4 +1,44 @@
 package interface_adapter.view_credibility;
 
-public class ViewCredibilityDetailsPresenter {
+import use_case.view_credibility.ViewCredibilityDetailsOutputBoundary;
+import use_case.view_credibility.ViewCredibilityDetailsOutputData;
+
+public class ViewCredibilityDetailsPresenter implements ViewCredibilityDetailsOutputBoundary {
+
+    private final ViewCredibilityDetailsViewModel viewModel;
+
+    public ViewCredibilityDetailsPresenter(ViewCredibilityDetailsViewModel viewModel) {
+        this.viewModel = viewModel;
+    }
+
+    @Override
+    public void prepareSuccessView(ViewCredibilityDetailsOutputData outputData) {
+        ViewCredibilityDetailsState state = viewModel.getState();
+
+        state.setTitle(outputData.getTitle());
+        state.setSource(outputData.getSource());
+        state.setUrl(outputData.getUrl());
+
+        state.setSourceScore(outputData.getSourceScore());
+        state.setSentimentScore(outputData.getSentimentScore());
+        state.setClaimConfidence(outputData.getClaimConfidence());
+        state.setOverallTrust(outputData.getOverallTrust());
+        state.setLevel(outputData.getLevel());
+        state.setRationale(outputData.getRationale());
+
+        state.setWeightSource(outputData.getWeightSource());
+        state.setWeightSentiment(outputData.getWeightSentiment());
+        state.setWeightClaim(outputData.getWeightClaim());
+
+        state.setError(null);
+
+        viewModel.firePropertyChange("details");
+    }
+
+    @Override
+    public void prepareFailView(String message) {
+        ViewCredibilityDetailsState state = viewModel.getState();
+        state.setError(message);
+        viewModel.firePropertyChange("error");
+    }
 }
