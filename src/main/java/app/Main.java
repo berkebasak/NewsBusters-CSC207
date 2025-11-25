9157be80de5ac469dfc893e9558a4416c42b16ea
Halomaster0 <Halomaster2006lol@gmail.com>
2025-11-25 18:26:19 -0500

    Fixed Credibility Generation flow for PR#10

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -16,7 +16,7 @@ public class Main {
                 .addSaveArticleUseCase()
                 .addSearchNewsUseCase()
                 .addDiscoverPageView()
-                .addDiscoverPageView()
+                .addCredibilityUseCases()
                 .addDiscoverPageUseCase()
                 .addProfileView()
                 .addProfileUseCase()
