63832b7d985c822ee18914a428c2918dafa7d62c
Muhammad Zendy Zainuddin <zendyzainuddin425@gmail.com>
2025-11-30 15:33:25 -0500

    Added an alternative constructor for SetPreferenceInputData

--- src/main/java/use_case/set_preferences/SetPreferencesInputData.java
+++ src/main/java/use_case/set_preferences/SetPreferencesInputData.java
@@ -1,11 +1,14 @@
 package use_case.set_preferences;
 
-import entity.User;
 import entity.UserPreferences;
 
 public class SetPreferencesInputData {
     String username;
     UserPreferences userPreferences;
+
+    public SetPreferencesInputData(String username) {
+        this.username = username;
+    }
 
     public SetPreferencesInputData(String username, UserPreferences userPreferences) {
         this.username = username;
