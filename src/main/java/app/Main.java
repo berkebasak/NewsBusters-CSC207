0294bb0621b6526e8ce9870e4eb4859705385705
Halomaster0 <Halomaster2006lol@gmail.com>
2025-11-30 23:02:45 -0500

    Finalized main implementing filter call.

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -11,6 +11,7 @@ public class Main {
                 .addSignupView()
                 .addTopHeadlinesView()
                 .addTopHeadlinesUseCase()
+                .addSortHeadlinesUseCase()
                 .addLoginUseCase()
                 .addSignupUseCase()
                 .addSaveArticleUseCase()
